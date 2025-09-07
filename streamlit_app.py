0acbce93b88059af544b0b4d6b6c9bb1ee631406
farishadi18-stack <farishadi18@gmail.com>
2025-09-08 00:39:27 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ slope_angle = st.number_input("Slope Angle (°)", min_value=15.0, max_value=90.0
 try:
     df = pd.read_csv("new treated slope.xlsx")
 except:
-    st.error("❌ Dataset not found. Please upload 'new treated slope.slsx' to your repo.")
+    st.error("❌ Dataset not found. Please upload 'new treated slope.xlsx' to your repo.")
     st.stop()
 
 # ----------------------------
